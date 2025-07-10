f381924a2cf145d8fae5ab3c617ff9aa9d1b45ba
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-07-10 18:14:16 -0300

    feat: adicionado novas rotas

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,13 +1,16 @@
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 import { db } from "../../db/connection.ts"
 import { schema } from "../../db/schema/index.ts"
+import { count, eq } from "drizzle-orm"
 
 export const getRoomsRoute: FastifyPluginCallbackZod= async (app) => {
     app.get('/rooms', async () => {
         const result = await db.select({
             id: schema.rooms.id,
             name: schema.rooms.name,
-        }).from(schema.rooms).orderBy(schema.rooms.createAt)
+            createAt: schema.rooms.createAt,
+            questionsCount: count(schema.questions.id)
+        }).from(schema.rooms).leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id)).groupBy(schema.rooms.id, schema.rooms.name).orderBy(schema.rooms.createAt)
 
         return result
     })
